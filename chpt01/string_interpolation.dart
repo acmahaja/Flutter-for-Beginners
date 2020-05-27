dd50b4d8aab6e21b3a6bbf2e98749a3bf5a1aea5
acmahaja <acmahaja@gmail.com>
2020-05-27 16:30:24 -0400

    Added string manipulation

---
+++ chpt01/string_interpolation.dart
@@ -0,0 +1,8 @@
+main() {
+  String someString = "This is a String";
+  print("The string value is: $someString");
+  // prints The Strig value is: This is a String
+
+  print("The length of the string is ${someString.length}");
+  // prints the length of the string is: 16
+}
