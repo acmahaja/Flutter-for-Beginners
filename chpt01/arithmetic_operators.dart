e5fde335d3be1f83033024584ca466739dcd07f3
acmahaja <acmahaja@gmail.com>
2020-05-27 15:31:36 -0400

    Added arithmetic operators

---
+++ chpt01/arithmetic_operators.dart
@@ -0,0 +1,23 @@
+main(List<String> args) {
+  double one = 10;
+  double two  = 20;
+  double calc = one + two;
+  print('one + two = $calc');
+
+  calc = one - two;
+  print('one - two = $calc');
+
+  calc = one * two;
+  print('one * two = $calc');
+
+  calc = one/two;
+  print('one / two = $calc');
+
+  calc = one % two;
+  print('one % two = $calc');
+
+  calc = -two;
+  print('-two = $calc');
+
+
+}
