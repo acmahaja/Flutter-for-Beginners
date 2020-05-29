94d4895e2f142b5d3a882f1bb434cd7465d69f04
acmahaja <acmahaja@gmail.com>
2020-05-29 18:06:16 -0400

    Added extra class inheritance example

--- chpt02/classes_constructors.dart
+++ chpt02/classes_constructors.dart
@@ -17,4 +17,10 @@ void main() {
   somePerson1.firstName = "Wayne";
   somePerson1.lastName = "Bruce";
   print(somePerson1.getFullName());
+
+  Person somePerson2 = new Person();
+  somePerson1.firstName = "Hal";
+  somePerson1.lastName = "Jordan";
+  print(somePerson2.getFullName());
+
 }
