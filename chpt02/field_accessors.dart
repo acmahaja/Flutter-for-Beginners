27689b05e90c67ee549db2bdc00578d0fa4e653d
acmahaja <acmahaja@gmail.com>
2020-06-25 12:57:29 -0400

    Updated setter example field accessors to include setters

--- chpt02/field_accessors.dart
+++ chpt02/field_accessors.dart
@@ -7,16 +7,23 @@ class Person {
   Person.anonymous() {}
 
   String get fullName => "$firstName $lastName";
-  String get initials => "$firstName[0] $lastName[0].";
-  
+  String get initials => "${firstName[0]}. ${lastName[0]}.";
+
+  set fullName(String fullName) {
+    var parts = fullName.split(" ");
+    this.firstName = parts.first;
+    this.lastName = parts.last;
+  }
 }
 
 main() {
   Person somePerson = new Person("clark", "kent");
-  
   print(somePerson.fullName);
   print(somePerson.initials);
 
-  //  somePerson.fullName = "peter parker";
+  somePerson.fullName = "peter parker";
+  print(somePerson.fullName);
+  print(somePerson.initials);
+
   //  we have not defined a setter fullName so it doesnt compile
 }
